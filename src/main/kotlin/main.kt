a6103d8c7c3089a87ddd71abadc11083f54365a4
BrunoMCerqueira <brunocerqueirasd@gmail.com>
2022-08-04 23:58:05 -0300

    Add account details.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,4 +2,12 @@ fun main() {
     var owner = "Bruno"
     println("Bem vindo ao Bytebank")
     println("Titular $owner")
+
+    val account = 187
+    var balance = 10.0
+
+    balance += 12
+    println("conta do titular $account")
+    println("Saldo $balance")
+
 }
